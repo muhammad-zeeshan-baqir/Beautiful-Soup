1a99e4fce6917fc19fcf58e7fe750bc2779330e0
Muhammad Zeeshan Baqir <muhammadzeeshanbaqir@gmail.com>
2022-01-28 12:44:37 +0500

    add print statment for pinting test

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ url = 'https://themoviesflix.co'
 z = requests.get(url)
 html_content = z.content
 print(html_content)
-
+print('test')
 soup = BeautifulSoup(html_content, 'html.parser')
 print(soup.prettify())
