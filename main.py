4110c8cfdc43b4e0a18882d37ee44b6d1b95eee5
Muhammad Zeeshan Baqir <muhammadzeeshanbaqir@gmail.com>
2022-01-28 12:37:36 +0500

    first commit for RPA

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+url = 'https://themoviesflix.co'
+
+z = requests.get(url)
+html_content = z.content
+print(html_content)
+
+soup = BeautifulSoup(html_content, 'html.parser')
+print(soup.prettify())
